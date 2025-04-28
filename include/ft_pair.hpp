1c8692d141184f78223910c275b725e1ccb5f40f
seungwoo7050 <seungwoo7050@naver.com>
2025-04-28 17:47:29 +0900

    feat(pair): 값 쌍과 관계 연산 구현

---
+++ include/ft_pair.hpp
@@ -0,0 +1,83 @@
+#ifndef FT_PAIR_HPP
+# define FT_PAIR_HPP
+
+namespace ft
+{
+	template <class T1, class T2>
+	struct pair
+	{
+		typedef T1 first_type;
+		typedef T2 second_type;
+
+		first_type first;
+		second_type second;
+
+		pair() : first(), second()
+		{
+		}
+
+		pair(const first_type& a, const second_type& b) : first(a), second(b)
+		{
+		}
+
+		template <class U, class V>
+		pair(const pair<U, V>& other) : first(other.first), second(other.second)
+		{
+		}
+
+		pair& operator=(const pair& other)
+		{
+			if (this != &other)
+			{
+				first = other.first;
+				second = other.second;
+			}
+			return *this;
+		}
+	};
+
+	template <class T1, class T2>
+	bool operator==(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return lhs.first == rhs.first && lhs.second == rhs.second;
+	}
+
+	template <class T1, class T2>
+	bool operator!=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return !(lhs == rhs);
+	}
+
+	template <class T1, class T2>
+	bool operator<(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return lhs.first < rhs.first
+			|| (!(rhs.first < lhs.first) && lhs.second < rhs.second);
+	}
+
+	template <class T1, class T2>
+	bool operator<=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return !(rhs < lhs);
+	}
+
+	template <class T1, class T2>
+	bool operator>(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return rhs < lhs;
+	}
+
+	template <class T1, class T2>
+	bool operator>=(const pair<T1, T2>& lhs, const pair<T1, T2>& rhs)
+	{
+		return !(lhs < rhs);
+	}
+
+	template <class T1, class T2>
+	pair<T1, T2> make_pair(T1 first, T2 second)
+	{
+		return pair<T1, T2>(first, second);
+	}
+}
+
+#endif
