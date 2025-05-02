80e169e832122ae4178965b7f8c46a19e6d9da72
seungwoo7050 <seungwoo7050@naver.com>
2025-05-02 21:28:16 +0900

    feat(headers): 공용 도구와 순차 컨테이너 통합 헤더 추가

---
+++ include/ft_containers.hpp
@@ -0,0 +1,11 @@
+#ifndef FT_CONTAINERS_HPP
+# define FT_CONTAINERS_HPP
+
+# include "ft_algorithm.hpp"
+# include "ft_iterator.hpp"
+# include "ft_pair.hpp"
+# include "ft_stack.hpp"
+# include "ft_type_traits.hpp"
+# include "ft_vector.hpp"
+
+#endif
