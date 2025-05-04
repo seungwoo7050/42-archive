7ae6f88861e364d0e3817b9375c75ea2d9fc1c3d
seungwoo7050 <seungwoo7050@naver.com>
2025-05-04 12:47:26 +0900

    test(map): 삽입·검색·삭제 결과를 표준 map과 비교

--- tests/test_containers.cpp
+++ tests/test_containers.cpp
@@ -1,5 +1,7 @@
 #include <cstdlib>
 #include <iostream>
+#include <map>
+#include <sstream>
 #include <stack>
 #include <stdexcept>
 #include <string>
@@ -119,6 +121,65 @@ namespace
 		require((fta == ftb) == (stda == stdb), "stack equality compare");
 		require((fta < ftb) == (stda < stdb), "stack less compare");
 	}
+
+	template <class FtMap, class StdMap>
+	void compare_map(const FtMap& ftm, const StdMap& stdm,
+		const std::string& label)
+	{
+		require(ftm.size() == stdm.size(), label + " size");
+		typename FtMap::const_iterator fit = ftm.begin();
+		typename StdMap::const_iterator sit = stdm.begin();
+		for (; fit != ftm.end() && sit != stdm.end(); ++fit, ++sit)
+		{
+			require(fit->first == sit->first, label + " key order");
+			require(fit->second == sit->second, label + " mapped value");
+		}
+		require(fit == ftm.end() && sit == stdm.end(), label + " end");
+	}
+
+	void test_map()
+	{
+		ft::map<int, std::string> ftm;
+		std::map<int, std::string> stdm;
+		int keys[] = {8, 3, 10, 1, 6, 14, 4, 7, 13, 6, 8};
+		for (std::size_t i = 0; i < sizeof(keys) / sizeof(keys[0]); ++i)
+		{
+			std::ostringstream oss;
+			oss << "v" << keys[i];
+			ft::pair<ft::map<int, std::string>::iterator, bool> fr =
+				ftm.insert(ft::make_pair(keys[i], oss.str()));
+			std::pair<std::map<int, std::string>::iterator, bool> sr =
+				stdm.insert(std::make_pair(keys[i], oss.str()));
+			require(fr.second == sr.second, "map duplicate insert flag");
+		}
+		compare_map(ftm, stdm, "map insert order");
+
+		ftm[5] = "five";
+		stdm[5] = "five";
+		ftm[6] = "six";
+		stdm[6] = "six";
+		compare_map(ftm, stdm, "map operator[]");
+
+		require(ftm.find(7)->second == stdm.find(7)->second, "map find");
+		require(ftm.count(111) == stdm.count(111), "map count missing");
+		require(ftm.lower_bound(6)->first == stdm.lower_bound(6)->first,
+			"map lower_bound");
+		require(ftm.upper_bound(6)->first == stdm.upper_bound(6)->first,
+			"map upper_bound");
+		require(ftm.equal_range(6).first->first == stdm.equal_range(6).first->first,
+			"map equal_range first");
+
+		ftm.erase(3);
+		stdm.erase(3);
+		ftm.erase(ftm.find(10));
+		stdm.erase(stdm.find(10));
+		compare_map(ftm, stdm, "map erase");
+
+		ft::map<int, std::string> ftcopy(ftm.begin(), ftm.end());
+		std::map<int, std::string> stdcopy(stdm.begin(), stdm.end());
+		compare_map(ftcopy, stdcopy, "map range constructor");
+		require(ftcopy == ftm, "map equality");
+	}
 }
 
 int main()
@@ -126,6 +187,7 @@ int main()
 	test_utilities();
 	test_vector();
 	test_stack();
+	test_map();
 	std::cout << "ft_containers checks passed" << std::endl;
 	return 0;
 }
